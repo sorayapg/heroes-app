b44539b5b6725c69501d6af9d0eef2f21a11a068
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-01-19 18:30:50 +0100

    paquete de query-string, actualizacion del searchPage, e implementacion del useForm

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,29 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from 'query-string';
+import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
 
 
 export const SearchPage = () => {
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q = '' }= queryString.parse( location.search);
+    
+
+    const { searchText, onInputChange } = useForm({
+        searchText: '',
+    });
+
+    const onSearchSubmit = (event) => {
+        event.preventDefault();
+        if ( searchText.trim().length <= 1) return;
+
+        navigate(`?q=${ searchText }`);
+        
+    }
+
     return (
 
         <>
@@ -12,13 +34,15 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form>
+                    <form onSubmit={ onSearchSubmit }>
                         <input 
                             type="text" 
                             placeholder="Search a hero" 
                             className="form-control" 
                             name="searchText"
                             autoComplete="off"
+                            value={ searchText }
+                            onChange={ onInputChange }
                         />
                         <button className="btn btn-outline-info mt-1">
                             Serach
@@ -32,7 +56,7 @@ export const SearchPage = () => {
                         Search a hero 
                     </div>
                     <div className="alert alert-danger">
-                        There's no <b>results</b> 
+                        No hero with <b>{ q }</b> 
                     </div>
                     {/* <HeroCard /> */}
                 </div>
