8c11241222539e62aeb06f8a35bdff787a224efd
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-01-19 17:23:33 +0100

    Actualizacion del SearchPage y el component NavBar

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,42 @@
+import { HeroCard } from "../components";
 
 
 export const SearchPage = () => {
     return (
-        <h1>SearchPage</h1>
+
+        <>
+        
+            <h1>Search</h1>
+            <hr />
+            <div className="row">
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <hr />
+                    <form>
+                        <input 
+                            type="text" 
+                            placeholder="Search a hero" 
+                            className="form-control" 
+                            name="searchText"
+                            autoComplete="off"
+                        />
+                        <button className="btn btn-outline-info mt-1">
+                            Serach
+                        </button>
+                    </form>
+                </div>
+                <div className="col-7">
+                    <h4>Results</h4>
+                    <hr />
+                    <div className="alert alert-info">
+                        Search a hero 
+                    </div>
+                    <div className="alert alert-danger">
+                        There's no <b>results</b> 
+                    </div>
+                    {/* <HeroCard /> */}
+                </div>
+            </div>
+        </>
     )
 }
