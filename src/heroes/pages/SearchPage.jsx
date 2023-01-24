ce42e245a74049af544b8374566efa72121305dd
Soraya Povedano Gallardo <s.povedanogallardo@dxc.com>
2023-01-24 13:18:58 +0100

    Actualizacion del SearchPage

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -25,7 +25,7 @@ export const SearchPage = () => {
     const onSearchSubmit = (event) => {
         event.preventDefault();
         //if ( searchText.trim().length <= 1) return;
-
+       
         navigate(`?q=${ searchText }`);
         
     }
@@ -40,7 +40,7 @@ export const SearchPage = () => {
                 <div className="col-5">
                     <h4>Searching</h4>
                     <hr />
-                    <form onSubmit={ onSearchSubmit }>
+                    <form onSubmit={ onSearchSubmit } aria-label="form">
                         <input 
                             type="text" 
                             placeholder="Search a hero" 
@@ -72,7 +72,7 @@ export const SearchPage = () => {
                     <div className="alert alert-info animate__animated animate__fadeIn" style={{ display: showSearch ? '' : 'none' }}>
                         Search a hero 
                     </div>
-                <div className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
+                    <div aria-label="alert-danger" className="alert alert-danger animate__animated animate__fadeIn" style={{ display: showError ? '' : 'none' }}>
                         No hero with <b>{ q }</b>
                     </div>
                     
